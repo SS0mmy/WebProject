060054bb77377b4b5ea5df213c2fa6f6356c0d13
eoc940 <hgk0000@naver.com>
2021-04-26 23:46:44 +0900

    회원가입, 로그인, 로그아웃, 정보수정 기능 구현

---
+++ VeganProject/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a href = "registerForm.jsp">회원가입하기</a>
+<a href ="loginForm.jsp">로그인하기</a>
+<a href ="updateForm.jsp">회원수정하기</a>
+<a href ="deleteForm.jsp">회원삭제하기</a>
+<a href ="logout.do">로그아웃하기</a>
+
+</body>
+</html>
--- VeganProject/src/com/team1/vegan/servlet/controller/HandlerMapping.java
+++ VeganProject/src/com/team1/vegan/servlet/controller/HandlerMapping.java
@@ -11,9 +11,19 @@ public class HandlerMapping {
 	//Component를 생성...command하는 기능...factory 본연의 역할
 	public Controller createController(String command) { // controller에서는 인터페이스 부모 타입으로 리턴
 		Controller controller = null;
-//		if(command.equals("find.do")) {
-//			controller = new FindController();
-//		}
+		if(command.equals("register.do")) {
+			controller = new RegisterController();
+		}if(command.equals("login.do")) {
+			controller = new LoginController();
+		}if(command.equals("update.do")) {
+			controller = new UpdateController();
+		}if(command.equals("delete.do")) {
+			controller = new DeleteController();
+		}if(command.equals("logout.do")) {
+			controller = new LogoutController();
+		}if(command.equals("idCheck.do")) {
+			controller = new IdCheckController();
+		}
 		return controller;
 	}
 }
